64349f130aa1ab9446b086338bd4aa3ee7eddca7
@wpp/m4 <lynusx@hotmail.com>
2026-02-07 08:50:30 +0800

    修复夜班时间段：添加缺失的 21,22,23 点

--- lib/models/el_collection_task.dart
+++ lib/models/el_collection_task.dart
@@ -30,7 +30,7 @@ extension ShiftTypeExtension on ShiftType {
       case ShiftType.dayShift:
         return [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
       case ShiftType.nightShift:
-        return [20, 0, 1, 2, 3, 4, 5, 6, 7];
+        return [20, 21, 22, 23, 0, 1, 2, 3, 4, 5, 6, 7];
     }
   }
 }
